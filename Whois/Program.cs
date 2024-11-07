a5ad8c2f4836065adffc27ceaaf975cd3d8cfb25
Hamza-Liaqa-t <h.liaqat-2023@hull.ac.uk>
2024-11-07 02:00:26 +0000

    Add project files.

---
+++ Whois/Program.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+
+string connStr = "server=localhost;user=root;database=umdb;port=3306;password=Sausage123";
+MySqlConnection conn = new MySqlConnection(connStr);
+
+Console.WriteLine("Enter Prompt");
+string prompt = Console.ReadLine();
+
+//put the if statement in while loop
+while (true)
+{
+    if (prompt != null)
+    {
+        try
+        {
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
+
+            string sql = "SELECT * FROM login";
+            MySqlCommand cmd = new MySqlCommand(prompt, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " -- " + rdr[1]); //+ " -- " + rdr[2] + " -- " + rdr[3] + " -- " + rdr[4]);
+            }
+            Console.WriteLine("Done.");
+            Console.WriteLine("Enter Prompt or press enter to exit");
+            string repeat = Console.ReadLine();
+            if (repeat == "")
+            {
+                break;
+                rdr.Close();
+            }
+            else
+            {
+                prompt = repeat;
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        conn.Close();
+    }
+}
+
+
+
+
+
+
